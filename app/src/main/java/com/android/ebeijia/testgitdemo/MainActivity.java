45beab02e5a3f46a2ad13f024e2bad0ffe68a589
qidong <527213991@qq.com>
2018-01-25 14:07:17 +0800

    添加测试代码

--- app/src/main/java/com/android/ebeijia/testgitdemo/MainActivity.java
+++ app/src/main/java/com/android/ebeijia/testgitdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.ebeijia.testgitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test();
+
+    }
+
+    public void test()
+    {
+        Log.e("QDTest","lalala");
     }
 }
